48498f8497d064dcd5a6e3e9f675190f9f87ac61
Yuki Ito <github@risaton.net>
2025-12-19 07:02:05 +0900

    update renderer.ts

--- src/renderer.ts
+++ src/renderer.ts
@@ -34,6 +34,9 @@ console.log(
 
 const button1 = document.getElementById('button1');
 
+const input1 = document.getElementById('input1');
+
 button1.addEventListener('click', () => {
-    alert('Hello World!');
+    // alert('Hello World!');
+    alert(input1.value)
 })
